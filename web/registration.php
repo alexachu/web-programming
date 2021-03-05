50532242ac3e396d7394d909f9250e7cd5dbd55c
Student <Student@mcalab.edu>
2021-03-05 15:51:12 +0530

    commeted

---
+++ web/registration.php
@@ -0,0 +1,21 @@
+<html>
+<head>
+<title>general form</title>
+</head>
+<body bgcolor="yellow">
+<form action = "insert2.php" method = "POST">
+<table cellpadding="5px">
+<tr><td>Name:</td>
+<td><input type = "text" name = "name"  placeholder="Enter your Name" ></td></tr>
+<tr><td>Roll no:</td>
+<td><input type = "text" name = "rollno" placeholder="Enter your Rollno"></td></tr>
+<tr><td>Gender:</td>
+<td><input type = "text" name = "gender" placeholder="Enter Gender"></td></tr>
+<tr><td>Address:</td>
+<td><textarea name = "address" type = "textarea" placeholder="Enter your Address"></textarea></td></tr>
+<tr><td></td><td><input type = "Submit" name = "insert" value = "Save">
+<input type = "Reset" value = "Cancle"></td></tr>
+</table>
+</form>
+</body>
+</html>
